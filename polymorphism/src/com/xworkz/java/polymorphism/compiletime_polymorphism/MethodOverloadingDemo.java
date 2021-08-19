3b92e95f703711d507aa6bc63dc69c71b360797d
janeesha <asha.mp89@gmail.com>
2021-08-19 15:09:35 +0530

    first test commit

---
+++ polymorphism/src/com/xworkz/java/polymorphism/compiletime_polymorphism/MethodOverloadingDemo.java
@@ -0,0 +1,8 @@
+package com.xworkz.java.polymorphism.compiletime_polymorphism;
+
+public class MethodOverloadingDemo {
+	public static void main(String[] args) {
+		System.out.println("MethodOverloading");
+	}
+
+}
